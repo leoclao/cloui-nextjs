f6411be647965dec8782c36094e4144f00a7928f
leoclao <daoxuannhut@gmail.com>
2023-06-29 17:40:37 +0700

    #patch update sb title

--- src/stories/Title.stories.tsx
+++ src/stories/Title.stories.tsx
@@ -25,14 +25,14 @@ export const Default: Story = {
     size: 0,
     theme: 0
   },
-  argTypes: {
-    size: {
-      control: false,
-    },
-    theme: {
-      control: false,
-    }
-  }
+  // argTypes: {
+  //   size: {
+  //     control: false,
+  //   },
+  //   theme: {
+  //     control: false,
+  //   }
+  // }
 }
 
 // Theme: Primary
@@ -48,5 +48,131 @@ export const Primary: Story = {
     theme: {
       control: false,
     }
+  }
+}
+
+// Theme: Tertiary
+export const Tertiary: Story = {
+  args: {
+    ...Default.args,
+    theme: 2,
+  },
+  argTypes: {
+    // ...Default.argTypes.theme,
+    theme: {
+      control: false,
+    },
+  },
+}
+
+// Size: Tiny
+export const Tiny: Story = {
+  args: {
+    ...Default.args,
+    size: 0,
+  },
+  argTypes: {
+    size: {
+      control: false,
+    },
+  },
+}
+
+// Size: Smaller
+export const Smaller: Story = {
+  args: {
+    ...Default.args,
+    size: 1,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: Small
+export const Small: Story = {
+  args: {
+    ...Default.args,
+    size: 2,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: Medium
+export const Medium: Story = {
+  args: {
+    ...Default.args,
+    size: 3,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: Large
+export const Large: Story = {
+  args: {
+    ...Default.args,
+    size: 4,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: XLarge
+export const XLarge: Story = {
+  args: {
+    ...Default.args,
+    size: 5,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: XXLarge
+export const XXLarge: Story = {
+  args: {
+    ...Default.args,
+    size: 6,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: XXXLarge
+export const XXXLarge: Story = {
+  args: {
+    ...Default.args,
+    size: 7,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: Big
+export const Big: Story = {
+  args: {
+    ...Default.args,
+    size: 8,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
+  }
+}
+
+// Size: Huge
+export const Huge: Story = {
+  args: {
+    ...Default.args,
+    size: 9,
+  },
+  argTypes: {
+    ...Tiny.argTypes,
   }
 }
